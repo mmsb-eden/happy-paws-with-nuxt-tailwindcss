47a82871e5c0002d9f1beb50717d5e4ea0ad94fc
Akayy <techakayy@gmail.com>
2023-11-13 21:32:34 +1100

    Minor update

--- pages/adopt/[id].vue
+++ pages/adopt/[id].vue
@@ -15,14 +15,17 @@
   const route = useRoute()
   const { id: petId } = route.params
 
-  const pet = await usePet(+petId)
+  const { fetchPet } = await usePet(+petId)
+
+  const { id, name, breed, sex, dateOfBirth, image, microchip, location } =
+    fetchPet()
 
   useServerSeoMeta({
-    description: () => pet.value?.name,
+    description: () => name || '',
   })
 
   useHead({
-    title: () => pet.value?.name,
+    title: () => name || '',
   })
 </script>
 <template>
@@ -37,8 +40,8 @@
               </NuxtLink>
             </div>
             <div class="text-center w-full lg:w-fit">
-              <h3 class="capitalize">{{ pet.name }}</h3>
-              <h6 class="uppercase">Animal ID: {{ pet.id }}</h6>
+              <h3 class="capitalize">{{ name }}</h3>
+              <h6 class="uppercase">Animal ID: {{ id }}</h6>
             </div>
             <div
               class="flex items-center justify-center py-4 space-x-4 text-center w-full lg:w-fit"
@@ -57,29 +60,25 @@
               class="flex flex-wrap justify-center text-center w-full lg:flex-nowrap lg:text-left"
             >
               <div class="w-full">
-                <NuxtImg
-                  :src="pet.image"
-                  class="rounded-xl w-full"
-                  :alt="pet.name"
-                />
+                <NuxtImg :src="image" class="rounded-xl w-full" :alt="name" />
               </div>
               <div class="py-8 w-full sm:px-16">
                 <ul>
                   <li class="mb-4">
                     <span class="font-semibold">Breed:</span>
-                    <span class="ml-2">{{ pet.breed }}</span>
+                    <span class="ml-2">{{ breed }}</span>
                   </li>
                   <li class="mb-4">
                     <span class="font-semibold">Age:</span>
-                    <span class="ml-2">{{ pet.dateOfBirth }}</span>
+                    <span class="ml-2">{{ dateOfBirth }}</span>
                   </li>
                   <li class="mb-4">
                     <span class="font-semibold">Gender:</span>
-                    <span class="ml-2">{{ pet.sex }}</span>
+                    <span class="ml-2">{{ sex }}</span>
                   </li>
                   <li class="mb-4">
                     <span class="font-semibold">Microchip No:</span>
-                    <span class="ml-2">{{ pet.microchip }}</span>
+                    <span class="ml-2">{{ microchip }}</span>
                   </li>
                   <li
                     class="flex items-center justify-center mb-4 lg:justify-start"
@@ -89,7 +88,7 @@
                       height="28px"
                       class="-ml-1 font-semibold"
                     ></BaseIcon>
-                    <span class="ml-2">{{ pet.location }}</span>
+                    <span class="ml-2">{{ location }}</span>
                   </li>
                   <li class="mb-4">
                     <span class="font-semibold">Includes:</span>
@@ -151,17 +150,16 @@
             <p>
               To adopt
               <span class="mx-1 text-primary-600 dark:text-primary-200"
-                >{{ pet.name }} </span
+                >{{ name }} </span
               ><span>or learn more, please visit our adoption centre at </span
               ><span class="mx-1 text-primary-600 dark:text-primary-200"
-                >{{ pet.location }}.</span
+                >{{ location }}.</span
               ><span
                 >Appointments are not required and walk-ins are welcome.</span
               >
             </p>
             <p class="mt-2">
-              Alternatively, please contact our {{ pet.location }} team directly
-              on
+              Alternatively, please contact our {{ location }} team directly on
               <span class="text-primary-600 dark:text-primary-200"
                 >03 2394 3284</span
               >
