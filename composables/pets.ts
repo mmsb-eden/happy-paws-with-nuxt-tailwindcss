ed8dcc62780f2b625e7bd68796270db2bc9c37a0
Akayy <techakayy@gmail.com>
2023-11-13 21:38:17 +1100

    Minor update

--- composables/pets.ts
+++ composables/pets.ts
@@ -2,7 +2,10 @@ export const usePets = async () => {
   // const { data, error } = await useFetch('/api/adopt/external-data')
   const { data, error } = await useFetch('/api/adopt/local-data')
 
-  const { allPets, somePets } = data.value
+  /* The above useFetch is a syntactic sugar of the below useAsyncData & $fetch combo */
+  // const { data, error } = await useAsyncData('pets', async () => {
+  //   return await $fetch('/api/store/local-data')
+  // })
 
   if (error.value) {
     throw createError({
@@ -11,5 +14,22 @@ export const usePets = async () => {
     })
   }
 
-  return { allPets, somePets }
+  const fetchBreed = (breed: string) => {
+    // // Optimize any image urls in the data contents
+    // const { optimizeImage } = useOptimizeImage()
+
+    return data.value.allPets.filter((pet) => !breed || pet.breed === breed)
+    // .map((pet) =>
+    //   pet.image
+    //     ? {
+    //         ...pet,
+    //         imageOptimized: optimizeImage(pet.image),
+    //       }
+    //     : pet,
+    // )
+  }
+
+  const { allPets, somePets } = data.value
+
+  return { allPets, somePets, fetchBreed }
 }
